2b43083be0cb71ffdd59522047b660eb6271b0e4
Rogério Saraceni <rsaraceni@gmail.com>
2022-10-19 21:28:09 -0300

    Add and config IMask

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./css/index.css"
+import IMask from "imask";
 
 const ccBgCoor01 = document.querySelector('.cc-bg svg > g g:nth-child(1) path')
 const ccBgCoor02 = document.querySelector('.cc-bg svg > g g:nth-child(2) path')
@@ -17,3 +18,29 @@ function setCardType(type) {
 }
 
 globalThis.setCardType = setCardType
+
+
+const securityCode = document.querySelector('#security-code')
+const securityCodePattern = {
+    mask: "0000"
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+
+const expirationDate = document.querySelector('#expiration-date')
+const expirationDatePattern = {
+    mask: "MM{/}YY",
+    blocks: {
+        MM: {
+            mask: IMask.MaskedRange,
+            from: 1,
+            to: 12
+        },
+        YY: {
+            mask: IMask.MaskedRange,
+            from: String(new Date().getFullYear()).slice(2),
+            to: String(new Date().getFullYear() + 10).slice(2)
+        },        
+    }
+}
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
